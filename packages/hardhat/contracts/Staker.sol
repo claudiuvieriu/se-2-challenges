46e7973023fe4ea0f6dfe46aedeba047f737ccef
claudiuvieriu <claudiuvieriu1995@gmail.com>
2023-10-11 18:07:05 +0300

    challenge 1 solution complete

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -8,24 +8,69 @@ contract Staker {
 
   ExampleExternalContract public exampleExternalContract;
 
+  mapping (address => uint256) public balances;
+  uint256 public constant threshold = 1 ether;
+
+  event Stake(address staker, uint256 amount);
+
+  // state machine - deadline in 4 days after contract deployment
+  uint256 public deadline = block.timestamp + 94 hours;
+
+  bool openForWithdraw = true;
+
   constructor(address exampleExternalContractAddress) {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
+  modifier allowWithdraw() {
+    require(openForWithdraw, "Can't withdraw");
+    _;
+  }
+
+  modifier notCompleted() {
+    require(!exampleExternalContract.completed(), "Canno't stake anymore!");
+    _;
+  }
+
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   // (Make sure to add a `Stake(address,uint256)` event and emit it for the frontend `All Stakings` tab to display)
-
+  function stake() public payable {
+    require (msg.value > 0, "Staked amount must be grater than 0");
+    balances[msg.sender] += msg.value;
+    emit Stake(msg.sender, msg.value);
+  }
 
   // After some `deadline` allow anyone to call an `execute()` function
   // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
-
+  function execute() public allowWithdraw notCompleted {
+    require (block.timestamp >= deadline, "Deadline not reached");
+    if(address(this).balance >= threshold) {
+      exampleExternalContract.complete{value: address(this).balance}();
+      openForWithdraw = false;
+    }
+  }
 
   // If the `threshold` was not met, allow everyone to call a `withdraw()` function to withdraw their balance
+  function withdraw() external allowWithdraw notCompleted {
+    require (balances[msg.sender] > 0, "You don't have anything staked");
 
+    //balance to withdraw
+    uint256 withdrawAmount = balances[msg.sender];
+
+    // Transfer the funds to the user
+    (bool success, ) = msg.sender.call{value: withdrawAmount}("");
+    require(success, "Withdrawal failed");
+
+    balances[msg.sender] = 0;
+  }
 
   // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
-
+  function timeLeft() public view returns (uint256) {
+    return block.timestamp >= deadline ? 0 : deadline - block.timestamp;
+  }
 
   // Add the `receive()` special function that receives eth and calls stake()
-
+  receive() external payable {
+    stake();
+  }
 }
--- packages/nextjs/generated/deployedContracts.ts
+++ packages/nextjs/generated/deployedContracts.ts
@@ -1,3 +1,313 @@
-const deployedContracts = null;
+const contracts = {
+  31337: [
+    {
+      chainId: "31337",
+      name: "localhost",
+      contracts: {
+        ExampleExternalContract: {
+          address: "0x9A676e781A523b5d0C0e43731313A708CB607508",
+          abi: [
+            {
+              inputs: [],
+              name: "complete",
+              outputs: [],
+              stateMutability: "payable",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "completed",
+              outputs: [
+                {
+                  internalType: "bool",
+                  name: "",
+                  type: "bool",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+          ],
+        },
+        Staker: {
+          address: "0x0B306BF915C4d645ff596e518fAf3F9669b97016",
+          abi: [
+            {
+              inputs: [
+                {
+                  internalType: "address",
+                  name: "exampleExternalContractAddress",
+                  type: "address",
+                },
+              ],
+              stateMutability: "nonpayable",
+              type: "constructor",
+            },
+            {
+              anonymous: false,
+              inputs: [
+                {
+                  indexed: false,
+                  internalType: "address",
+                  name: "staker",
+                  type: "address",
+                },
+                {
+                  indexed: false,
+                  internalType: "uint256",
+                  name: "amount",
+                  type: "uint256",
+                },
+              ],
+              name: "Stake",
+              type: "event",
+            },
+            {
+              inputs: [],
+              name: "deadline",
+              outputs: [
+                {
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "exampleExternalContract",
+              outputs: [
+                {
+                  internalType: "contract ExampleExternalContract",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "execute",
+              outputs: [],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "stake",
+              outputs: [],
+              stateMutability: "payable",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "threshold",
+              outputs: [
+                {
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "timeLeft",
+              outputs: [
+                {
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "withdraw",
+              outputs: [],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              stateMutability: "payable",
+              type: "receive",
+            },
+          ],
+        },
+      },
+    },
+  ],
+  11155111: [
+    {
+      chainId: "11155111",
+      name: "sepolia",
+      contracts: {
+        ExampleExternalContract: {
+          address: "0x83e72C18Aee200C35BD28D4086671eb772D6623e",
+          abi: [
+            {
+              inputs: [],
+              name: "complete",
+              outputs: [],
+              stateMutability: "payable",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "completed",
+              outputs: [
+                {
+                  internalType: "bool",
+                  name: "",
+                  type: "bool",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+          ],
+        },
+        Staker: {
+          address: "0x9B40bAdb69764869Fe5555957B9094B5B97A76bb",
+          abi: [
+            {
+              inputs: [
+                {
+                  internalType: "address",
+                  name: "exampleExternalContractAddress",
+                  type: "address",
+                },
+              ],
+              stateMutability: "nonpayable",
+              type: "constructor",
+            },
+            {
+              anonymous: false,
+              inputs: [
+                {
+                  indexed: false,
+                  internalType: "address",
+                  name: "staker",
+                  type: "address",
+                },
+                {
+                  indexed: false,
+                  internalType: "uint256",
+                  name: "amount",
+                  type: "uint256",
+                },
+              ],
+              name: "Stake",
+              type: "event",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "address",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              name: "balances",
+              outputs: [
+                {
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "deadline",
+              outputs: [
+                {
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "exampleExternalContract",
+              outputs: [
+                {
+                  internalType: "contract ExampleExternalContract",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "execute",
+              outputs: [],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "stake",
+              outputs: [],
+              stateMutability: "payable",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "threshold",
+              outputs: [
+                {
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "timeLeft",
+              outputs: [
+                {
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "withdraw",
+              outputs: [],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              stateMutability: "payable",
+              type: "receive",
+            },
+          ],
+        },
+      },
+    },
+  ],
+} as const;
 
-export default deployedContracts;
+export default contracts;
